c7c2c7f7eb33b8ec22420e08453485e75c73e664
SandeepT <tstsandeep@gmail.com>
2026-01-11 13:17:07 +0530

    Second Commit

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("Hello, World!")
+import cv2
+import numpy as np
+from PIL import Image
+from util import get_limits
+
+yellow = [0,255,255]
+
+cap = cv2.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+    hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    lowerLimit, upperLimit = get_limits(color=yellow)
+
+    mask = cv2.inRange(hsvImage, lowerLimit, upperLimit)
+
+    mask_ = Image.fromarray(mask)
+    bbox = mask_.getbbox()
+    if bbox is not None:
+        X1, y1, x2, y2 = bbox
+        frame = cv2.rectangle(frame, (X1, y1), (x2, y2), (255, 0, 0), 2)
+
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
